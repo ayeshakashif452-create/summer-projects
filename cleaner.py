078d08b8a66c6de0e8d2d5f6dde35b5b522d810c
Ayesha Kashif <ayeshakashif452@gmail.com>
2026-06-04 14:06:27 -0400

    add log file feature

--- cleaner.py
+++ cleaner.py
@@ -1,5 +1,6 @@
 import os #work with files
 import shutil #move files
+import datetime
 
 downloads = "C:/Users/ayesh/Downloads"
 #this is just a path to all downloads
@@ -18,7 +19,7 @@ folders = {
 for filename in os.listdir(downloads):
     filepath= os.path.join(downloads, filename)
 
-    if os.path.isfile(filepath):
+    if os.path.isfile(filepath) and filename != "sorter_log.txt":
         ending= os.path.splitext(filename)[1].lower()
         
         for folder_name, endings in folders.items():
@@ -26,6 +27,10 @@ for filename in os.listdir(downloads):
                 folder_path = os.path.join(downloads, folder_name)
                 os.makedirs(folder_path, exist_ok=True)
                 shutil.move(filepath, folder_path)
+                log_entry = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} | {filename} - {folder_name}\n"  # added
+                print(log_entry.strip())  
+                with open(os.path.join(downloads, "sorter_log.txt"), "a") as log:  # added
+                    log.write(log_entry)  
                 break
 
 print("Downloads folder cleaned!")
